abbda2762765ae89bda9bee2e754df1d2cca369a
Andrei Vacariu <andrei@avacariu.me>
2017-11-10 20:03:07 -0800

    Handle more instructions

--- src/pycfg/cfg.py
+++ src/pycfg/cfg.py
@@ -1,72 +1,97 @@
 import dis
+import functools
+from collections import namedtuple
+
+from . import ops
 
 
-_conditional_jumps = {
-    'FOR_ITER',
-    'JUMP_IF_FALSE_OR_OP',
-    'JUMP_IF_TRUE_OR_POP',
-    'POP_JUMP_IF_FALSE',
-    'POP_JUMP_IF_TRUE',
-}
-
-_unconditional_jumps = {
-    'JUMP_FORWARD',
-    'JUMP_ABSOLUTE',
-    'CONTINUE_LOOP',
-}
-
-_new_block_instructions = {
-    'SETUP_LOOP',
-    'SETUP_EXCEPT',
-    'SETUP_FINALLY',
-    'SETUP_WITH',
-}
-
-_irregular_instructions = (_conditional_jumps |
-                           _unconditional_jumps |
-                           _new_block_instructions)
+class InvalidInstruction(Exception):
+    pass
 
 
-def _is_regular(instr):
-    return instr.opname not in _irregular_instructions
+def _is_boring(instr):
+    return instr.opname in ops.boring_opnames
 
 
 class CFG:
     def __init__(self, code):
         bytecode = dis.Bytecode(code)
+        self.basic_blocks = {}
 
-        basic_blocks = {}
-
-        bb = BasicBlock()
-
-        blockstack = []
+        ctx = Context(bytecode)
 
         for instr in bytecode:
-            if not bb or _is_regular(instr):
-                bb.instructions.append(instr)
+            bb = BasicBlock([instr])
+            self.basic_blocks[instr.offset] = bb
 
-            elif instr.opname in _new_block_instructions:
-                end_block_addr = instr.argval
-                blockstack.append(end_block_addr)
-
-            elif instr.opname == 'BREAK_LOOP':
-                pass
-
-            bb.instructions.append(instr)
-
-            if instr.opname in _conditional_jumps:
+            if _is_boring(instr):
+                # TODO: depend on the type of block we're in, we might also need
+                # to add the finally/except/WITH_CLEANUP_FINISH instructions as
+                # successors
                 bb.successors.append(instr.offset + 2)
 
-            bb.successors.append(instr.argval)
+            elif instr.opname == 'BREAK_LOOP':
+                jump_target = ctx.break_loop()
 
-            basic_blocks[bb.instructions[0].offset] = bb
+                bb.successors.append(jump_target)
 
-            bb = BasicBlock()
+            # Unconditional jumps
+            elif instr.opname in {'JUMP_FORWARD', 'JUMP_ABSOLUTE'}:
+                bb.successors.append(instr.argval)
 
-        if bb:
-            basic_blocks[bb.instructions[0].offset] = bb
+            # Conditional jumps
+            elif instr.opname in {'POP_JUMP_IF_TRUE', 'POP_JUMP_IF_FALSE',
+                                  'JUMP_IF_TRUE_OR_POP', 'JUMP_IF_FALSE_OR_POP',
+                                  'FOR_ITER'}:
+                bb.successors.append(instr.offset + 2)
+                bb.successors.append(instr.argval)
 
-        self.basic_blocks = basic_blocks
+            elif instr.opname == 'POP_BLOCK':
+                ctx.pop_block()
+                bb.successors.append(instr.offset + 2)
+
+            elif instr.opname == 'CONTINUE_LOOP':
+                bb.successors.append(instr.argval)
+
+            elif instr.opname == 'RETURN_VALUE':
+                # TODO: a return within a finally will always execute instead of
+                # the return anywhere else, so it's not always accurate to say
+                # that a return is always a leaf in the CFG
+                bb.successors = None
+
+            elif instr.opname == 'SETUP_FINALLY':
+                ctx.setup_finally(instr)
+                bb.successors.append(instr.offset + 2)
+
+            elif instr.opname == 'SETUP_EXCEPT':
+                ctx.setup_except(instr)
+                bb.successors.append(instr.offset + 2)
+
+            elif instr.opname == 'SETUP_LOOP':
+                ctx.setup_loop(instr)
+                bb.successors.append(instr.offset + 2)
+
+            elif instr.opname == 'SETUP_WITH':
+                ctx.setup_with(instr)
+                bb.successors.append(instr.offset + 2)
+
+            elif instr.opname == 'WITH_CLEANUP_START':
+                bb.successors.append(instr.offset + 2)
+
+            elif instr.opname == 'WITH_CLEANUP_FINISH':
+                bb.successors.append(instr.offset + 2)
+
+            else:
+                raise ValueError("Unhandled instruction: %r" % instr)
+
+    def __iter__(self):
+        return iter(self.basic_blocks.values())
+
+    def __getitem__(self, key):
+        return self.basic_blocks[key]
+
+    def __contains__(self, key):
+        return key in self.basic_blocks
 
 
 class BasicBlock:
@@ -92,3 +117,79 @@ class BasicBlock:
             return "{i.opname}\t{i.arg}\t({i.argval})".format(i=i)
 
         return "\n".join(map(repr_inst, self.instructions))
+
+
+class Block(namedtuple('Block', 'originator next_offset')):
+    pass
+
+
+def assert_blockstack(f):
+    @functools.wraps(f)
+    def wrapped(self, *args, **kwargs):
+        if not self.blockstack:
+            raise InvalidInstruction()
+
+        return f(self, *args, **kwargs)
+
+    return wrapped
+
+
+def check_instr(opname):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapped(self, instr, *args, **kwargs):
+            if instr.opname != opname:
+                raise ValueError(
+                    "Wrong instruction type. Expected '%s'. Found '%s'" % (opname, instr.opname)
+                )
+
+            return f(self, instr, *args, **kwargs)
+        return wrapped
+    return decorator
+
+
+class Context:
+    def __init__(self, bytecode):
+        self.bytecode = bytecode
+
+        self.blockstack = []
+
+    def _new_block(self, instr):
+        block = Block(instr, instr.argval)
+        self.blockstack.append(block)
+
+    @assert_blockstack
+    def break_loop(self):
+        # NOTE: the offset the break will jump to should be right after a
+        # POP_BLOCK, so we'll just pop the block here
+        inner_block = self.blockstack.pop()
+        return inner_block.next_offset
+
+    @assert_blockstack
+    def pop_block(self):
+        inner_block = self.blockstack[-1]
+
+        if inner_block.originator.opname == 'SETUP_WITH':
+            # with blocks will have a END_FINALLY instruction after the cleanup
+            # instructions, and we should let that one pop the block instead,
+            # since exceptions within the `with` block may cause this POP_BLOCK
+            # to not execute
+            return
+
+        self.block_stack.pop()
+
+    @check_instr('SETUP_LOOP')
+    def setup_loop(self, instr):
+        self._new_block(instr)
+
+    @check_instr('SETUP_FINALLY')
+    def setup_finally(self, instr):
+        self._new_block(instr)
+
+    @check_instr('SETUP_EXCEPT')
+    def setup_except(self, instr):
+        self._new_block(instr)
+
+    @check_instr('SETUP_WITH')
+    def setup_with(self, instr):
+        self._new_block(instr)
--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -1,6 +1,19 @@
 import unittest
 
+import pycfg
+
+
+def _f(x):
+    for i in range(x):
+        print(i**2)
+
+    return i
+
 
 class TestCFG(unittest.TestCase):
-    def test_1(self):
-        assert True
+    def test_all_successors_exist(self):
+        cfg = pycfg.CFG(_f.__code__)
+
+        for bb in cfg:
+            for bb_succ in bb.successors:
+                assert bb_succ in cfg
